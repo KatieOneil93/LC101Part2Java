38ca9f3b3ce31f628b5c17ffcbb880332f14e193
Kaitlyn O'Neil <katie@kaitlyns-mbp.attlocal.net>
2021-08-31 15:37:33 -0500

    finished ch4 exercicses

--- src/org/launchcode/java/exercises/ch4/school/Course.java
+++ src/org/launchcode/java/exercises/ch4/school/Course.java
@@ -1,4 +1,13 @@
 package org.launchcode.java.exercises.ch4.school;
 
+import java.util.ArrayList;
+
 public class Course {
+    //at least three fields
+    //name
+    private String name;
+    //list of students
+    private ArrayList<Student> students;
+    //number of credits earned from course
+    private int numOfCredits;
 }
---
+++ src/org/launchcode/java/exercises/ch4/school/TEacher.java
@@ -0,0 +1,23 @@
+package org.launchcode.java.exercises.ch4.school;
+
+public class TEacher {
+    //firstName, lastName, subject, yearsTeaching
+    private String firstName;
+    private String lastName;
+    private String subject;
+    private int yearsTeaching;
+
+
+    //getters and setters
+    public String getFirstName(){return this.firstNme;}
+    private void setFirstName(String firstName){this.firstName = firstName;}
+
+    public String getLastNam(){return this.lastName;}
+    public void setLastName(String lastName){this.lastName = lastName;}
+
+    public String getSubject(){return this.subject;}
+    public void setSubject(String subject){this.subject = subject;}
+
+    public int getYearsTeaching(){return this.yearsTeaching;}
+    public void setYearsTeaching(int yearsTeaching){this.yearsTeaching = yearsTeaching;}
+}
